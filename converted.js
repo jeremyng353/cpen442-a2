68e3ba746079a948d36b9f612198945b3d39fc1d
sammauns <maunsellsam@gmail.com>
2022-10-04 20:52:55 -0700

    scoreTextQgram added

--- converted.js
+++ converted.js
@@ -142,3 +142,17 @@ function keyShuffle(key) {
         key[j] = temp;
     }
 }
+
+function scoreTextQgram(text, len){
+    let i = 0;
+    temp = new Array(4);
+    let score = 0.0;
+    for(i = 0; i < len-3; i++){
+        temp[0] = text[i]-'A';
+        temp[1] = text[i+1]-'A';
+        temp[2] = text[i+2]-'A';
+        temp[3] = text[i+3]-'A';
+        score += qgram[17576*temp[0] + 676*temp[1] + 26*temp[2] + temp[3]];
+    }
+    return score;
+}
